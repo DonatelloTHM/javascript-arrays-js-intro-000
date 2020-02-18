2fd7014140548787dd40712244448069b594f251
Donat Pllana <donat.pllana@outlook.com>
2020-02-18 22:05:08 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -10,3 +10,24 @@ function destructivelyAddElementToBeginningOfArray(a,b)
   var c=[b,...a];
   return c;
 }
+function accessElementInArray(a,b)
+{
+  return a[b];
+}
+function destructivelyRemoveElementFromBeginningOfArray(a){
+  a.shift();
+  return a;
+}
+function removeElementFromBeginningOfArray(a){
+  a=a.slice(1);
+  return a;
+}
+function destructivelyRemoveElementFromEndOfArray(a)
+{
+  a.pop();
+  return a;
+}
+function removeElementFromEndOfArray(a){
+  a=a.slice(0, a.length - 1);
+
+}
