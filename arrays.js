d0ebd52966c444f6bfd42b085d7e1e249040a57c
Donat Pllana <donat.pllana@outlook.com>
2020-02-18 21:47:38 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,13 @@
 var chocolateBars=["snickers","hundred grand","kitkat","skittles"];
+
+function addElementToBeginningOfArray(a,b)
+{
+  a.unshift(b);
+  return a;
+}
+function destructivelyAddElementToBeginningOfArray(a,b)
+{
+  var c=[b,...a];
+  return c;
+}
+console.log(addElementToBeginningOfArray(chocolateBars,"Mule"))
